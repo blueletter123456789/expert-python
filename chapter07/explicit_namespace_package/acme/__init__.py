fba78502bcd5e00101e46a7a27e2be819bfa56b2
Shuhei Abe <blueletter123456789@gmail.com>
2023-03-01 08:47:03 +0900

    add chapter 07

---
+++ chapter07/explicit_namespace_package/acme/__init__.py
@@ -0,0 +1,4 @@
+__import__('pkg_resources').declare_namespace(__name__)
+
+# pkgutilは標準ライブラリのため、ポータビリティが高い
+# __path__ = __import__('pkgutil').extend_path(__path__, __name__)
---
+++ chapter07/explicit_namespace_package/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+
+# python3.3より古いバージョンの名前空間
+setup(
+    name='acme.templating',
+    packages=['acme.templating'],
+    namespace_packages=['acme'],
+)
